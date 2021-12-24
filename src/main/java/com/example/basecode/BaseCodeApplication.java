ebc85a8c80bc61d6f87bb26368ea766604019990
yejidaxuesheng <1398571775@qq.com>
2021-12-25 00:19:54 +0800

    添加了log配置
    log配置

--- src/main/java/com/example/basecode/BaseCodeApplication.java
+++ src/main/java/com/example/basecode/BaseCodeApplication.java
@@ -2,7 +2,6 @@ package com.example.basecode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class BaseCodeApplication {
 
---
+++ src/main/java/com/example/basecode/moduls/test/Test.java
@@ -0,0 +1,18 @@
+package com.example.basecode.moduls.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class Test {
+    @RequestMapping("/test")
+    public String test(){
+        log.info("12312");
+        return "123123";
+    }
+}
