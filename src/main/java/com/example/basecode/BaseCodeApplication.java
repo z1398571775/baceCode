4c1d046d5080ebe896f23ca55cc0084fa2dd9152
z1398571775 <39150828+z1398571775@users.noreply.github.com>
2021-12-24 16:54:57 +0800

    初始化环境
    初始化环境

---
+++ src/main/java/com/example/basecode/BaseCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.basecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basecode/BaseCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
